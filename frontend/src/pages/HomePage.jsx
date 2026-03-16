de84e2e627764693c4e0f6e9b6b5ac879c96c6e1
Fomekong Godwill <habrahamfomekong@gmail.com>
2026-03-16 03:52:29 +0100

    trigger vercel deployment

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -37,7 +37,7 @@ function HomePage() {
             <PackageIcon className="size-12" />
           </div>
           <div className="text-center space-y-2">
-            <h3 className="text-2xl font-semibold ">No products found</h3>
+            <h3 className="text-2xl font-semibold ">No products found!</h3>
             <p className="text-gray-500 max-w-sm">
               Get started by adding your first product to the inventory
             </p>
